1702210408f3e75dbb1e29fc50948706dda03c7c
81620288 <81620288@mocli10215.animaedu.intranet>
2018-03-02 22:10:43 -0300

    geometria

--- desweb_a01e1/src/teste/Geometria.java
+++ desweb_a01e1/src/teste/Geometria.java
@@ -13,6 +13,13 @@ public class Geometria {
 		Triangulo triangulo = new Triangulo(10.0,5.0);
 		Retangulo retangulo = new Retangulo(7.0,3.0);
 		Quadrado quadrado = new Quadrado(6.0);
+		
+		System.out.println(losango.area());
+		System.out.println(retangulo.area());
+		System.out.println(triangulo.area());
+		System.out.println(quadrado.area());
+		System.out.println(retangulo.diagonal());
+		System.out.println(quadrado.diagonal());
 	}
 
 }
